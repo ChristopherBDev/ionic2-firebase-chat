246ae7b92c75b377430939d8aba16848697448f3
ChristopherBaumg <christopherbaumg@gmail.com>
2017-10-06 11:47:46 -0300

    commit na master

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,30 +1,65 @@
+import { ChatPage } from './../pages/chat/chat';
 import { BrowserModule } from '@angular/platform-browser';
 import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
+import { HttpModule } from '@angular/http'
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+import { SignupPage } from './../pages/signup/signup';
+import { FirebaseAppConfig, AngularFireModule, AuthMethods, AuthProviders } from 'angularfire2';
+import { UserService } from '../providers/user/user.service';
+import { AuthService } from '../providers/auth/auth.service';
+import { SignPage } from '../pages/sign/sign';
+import { CustomLoggedHeaderComponent } from '../components/custom-logged-header/custom-logged-header.component';
+import { CaptalizePipe } from './../pipes/captalize/captalize.pipe';
+
+const firebaseAppConfig: FirebaseAppConfig = {
+  apiKey: "AIzaSyAMvAj5CJf7u9vY8CcTRpsZVyHGx_kUkAU",
+  authDomain: "ionic2-firebase-chat-a8e8c.firebaseapp.com",
+  databaseURL: "https://ionic2-firebase-chat-a8e8c.firebaseio.com",
+  storageBucket: "ionic2-firebase-chat-a8e8c.appspot.com",
+  messagingSenderId: "583404196031"
+};
+
+const firebaseAuthConfig = {
+  provider: AuthProviders.Custom,
+  method: AuthMethods.Password
+};
 
 @NgModule({
   declarations: [
+    CaptalizePipe,
+    ChatPage,
+    CustomLoggedHeaderComponent,
     MyApp,
-    HomePage
+    HomePage,
+    SignPage,
+    SignupPage
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    HttpModule,
+    IonicModule.forRoot(MyApp),
+    AngularFireModule.initializeApp(firebaseAppConfig, firebaseAuthConfig)
   ],
   bootstrap: [IonicApp],
   entryComponents: [
+    ChatPage,
+    CustomLoggedHeaderComponent,
     MyApp,
-    HomePage
+    HomePage,
+    SignPage,
+    SignupPage
   ],
   providers: [
+    AuthService,
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    { provide: ErrorHandler, useClass: IonicErrorHandler },
+    UserService,
   ]
 })
-export class AppModule {}
+export class AppModule { }
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,16 @@
+import { ChatPage } from './../chat/chat';
+import { SignPage } from './../sign/sign';
 import { Component } from '@angular/core';
+
 import { NavController } from 'ionic-angular';
+
+import { FirebaseListObservable } from 'angularfire2';
+
+import { AuthService } from './../../providers/auth/auth.service';
+
+import { SignupPage } from './../signup/signup';
+import { User } from '../../../models/user.model';
+import { UserService } from '../../providers/user/user.service';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +18,32 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  users: FirebaseListObservable<User[]>;
+  view: string = "chats";
+  
+  constructor(
+    public authService: AuthService,
+    public navCtrl: NavController,
+    public userService: UserService) {
 
+  }
+
+  ionViewCanEnter(): Promise<boolean> {
+    return this.authService.authenticated;
+  }
+
+  ionViewDidLoad() {
+    this.users = this.userService.users;
+  }
+
+  onSignup(): void {
+    this.navCtrl.push(SignupPage);
+  }
+
+  onChatCreate(user: User): void {
+    this.navCtrl.push(ChatPage, {
+      recipientUser: user
+    });
   }
 
 }
